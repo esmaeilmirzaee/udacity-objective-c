48b7c766fbebcc471c1ed647c639f8f06f012aca
Esmaeil MIRZAEE <esmaeilmirzaee@gmail.com>
2020-04-24 08:57:30 -0400

    Init

---
+++ Udacity-Objective-C/main.m
@@ -0,0 +1,17 @@
+//
+//  main.m
+//  Udacity-Objective-C
+//
+//  Created by Esmaeil MIRZAEE on 2020-04-24.
+//  Copyright © 2020 TheBeaver. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+int main(int argc, const char * argv[]) {
+    @autoreleasepool {
+        // insert code here...
+        NSLog(@"Hello, World!");
+    }
+    return 0;
+}
