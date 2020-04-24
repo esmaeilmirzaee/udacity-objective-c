ddf2a71988d300a1069fe09090c8aa3a2c5981dd
Esmaeil MIRZAEE <esmaeilmirzaee@gmail.com>
2020-04-24 09:20:53 -0400

    Add house class

---
+++ Udacity-Objective-C/House/House.h
@@ -0,0 +1,17 @@
+//
+//  House.h
+//  Udacity-Objective-C
+//
+//  Created by Esmaeil MIRZAEE on 2020-04-24.
+//  Copyright © 2020 TheBeaver. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+NS_ASSUME_NONNULL_BEGIN
+
+@interface House : NSObject
+
+@end
+
+NS_ASSUME_NONNULL_END
---
+++ Udacity-Objective-C/House/House.m
@@ -0,0 +1,15 @@
+//
+//  House.m
+//  Udacity-Objective-C
+//
+//  Created by Esmaeil MIRZAEE on 2020-04-24.
+//  Copyright © 2020 TheBeaver. All rights reserved.
+//
+
+#import "House.h"
+
+@implementation House
+
+
+
+@end
